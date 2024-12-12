c1307bcddc835a7eaa3c92896afba6851ffd944d
Ashish23jun <pandey.k.ashish.86@gmail.com>
2024-12-12 18:28:30 +0530

    new Change

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 // Listening PORT
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
